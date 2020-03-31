64e5fc4aeaf150d083b91d505202f42d2107e86e
Serge Karpiesz <serge.karpiesz@recruitee.com>
2020-03-31 13:07:28 +0200

    Allow elixir 1.8

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Ambassador.MixProject do
     [
       app: :ambassador,
       version: "0.1.0",
-      elixir: "~> 1.10",
+      elixir: "~> 1.8",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
