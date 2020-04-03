31cb7e6615ad4c2d76285ed53c9fed7c31bac465
Serge Karpiesz <serge.karpiesz@recruitee.com>
2020-04-03 19:07:53 +0200

    Install dialyxir

--- mix.exs
+++ mix.exs
@@ -28,8 +28,8 @@ defmodule Ambassador.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      # Dev tools
+      {:dialyxir, "~> 1.0", only: :dev, runtime: false}
     ]
   end
 end
