4915603278c78be1f225f560dc0d153ded3dbcdd
Serge Karpiesz <serge.karpiesz@recruitee.com>
2020-10-24 05:10:15 +0200

    Bump version

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule Cadastre.MixProject do
   use Mix.Project
 
-  @version "0.1.3"
+  @version "0.1.4"
   @description "A repository of languages, countries and country subdivisions from the iso-codes Debian package."
   @source_url "https://github.com/Recruitee/cadastre"
 
